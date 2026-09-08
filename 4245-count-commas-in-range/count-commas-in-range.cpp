63f47b7e2130516f0379d3c2ac98a95790398a6e
Darshann874 <darshannegi156@gmail.com>
2026-09-08 10:12:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        
+        if(n>=1000){
+            n=n-1000;
+            return n+1;
+        }
+        
+        return 0;
+    }
+};
