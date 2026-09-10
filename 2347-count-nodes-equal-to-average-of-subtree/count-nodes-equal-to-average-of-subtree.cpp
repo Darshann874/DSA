89d79d955db67d19bcaf8c90dd4605744075e69a
Darshann874 <darshannegi156@gmail.com>
2026-09-10 20:27:35 +0530

    Time: 7 ms (54.96%) | Memory: 15.9 MB (29.76%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -6,30 +6,48 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int findsum(TreeNode* root,int & c){
-        if(root==NULL)return 0;
-        c++;
-        int ls=findsum(root->left,c);
-        int rs=findsum(root->right,c);
-        return ls+rs+root->val;
+    int result;
+    pair<int,int> solve(TreeNode* root){
+        if(root==NULL)return {0,0};
+        
+        pair<int,int> ls=solve(root->left);
+        pair<int,int> rs=solve(root->right);
+
+        int ts=ls.first+rs.first+root->val;
+        int tc=ls.second + rs.second +1;
+        if(root->val==ts/tc){
+            result++;
+        }
+        return {ts,tc};
     }
+    // int findsum(TreeNode* root,int & c){
+    //     if(root==NULL)return 0;
+    //     c++;
+    //     int ls=findsum(root->left,c);
+    //     int rs=findsum(root->right,c);
+    //     return ls+rs+root->val;
+    // }
     int averageOfSubtree(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-        int res=0;
-        int c=0;
-        int sum=findsum(root,c);
-        if(root->val==sum/c){
-            res++;
-        }
-        res+=averageOfSubtree(root->left);
-        res+=averageOfSubtree(root->right);
-        return res;
+        // if(root==NULL){
+        //     return 0;
+        // }
+        // int res=0;
+        // int c=0;
+        // int sum=findsum(root,c);
+        // if(root->val==sum/c){
+        //     res++;
+        // }
+        // res+=averageOfSubtree(root->left);
+        // res+=averageOfSubtree(root->right);
+        // return res;
+        result=0;
+        solve(root);
+        return result;
     }
 };
